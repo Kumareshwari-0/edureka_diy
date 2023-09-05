09bce7e351248ab94a43ed2aeda1aa9720f30db4
Kumareshwari.k <141395010+Kumareshwari-0@users.noreply.github.com>
2023-09-05 12:36:41 +0530

    Add files via upload

---
+++ day-33/day33.js
@@ -0,0 +1,24 @@
+let array1 = [
+    "apple",
+    "Mango",
+    "Orange",
+    "Pine apple"
+];
+
+console.log(array1);
+
+for (element in array1);
+console.log(element);
+
+let obj1 = {
+    Empid: 1001,
+    MailId: "Dora@gmail.com",
+    Address: "Pinku street,Kovai"
+}
+
+console.log(obj1);
+
+const obj2 = new Set();
+obj2.add("10, 20");
+obj2.add("Nothing");
+console.log(obj2);
