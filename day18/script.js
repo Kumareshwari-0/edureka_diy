ed6197cd55a2aafd5975cad254bf3aea3212576e
Kumareshwari.k <141395010+Kumareshwari-0@users.noreply.github.com>
2023-09-28 16:41:45 +0530

    Add files via upload

---
+++ day18/script.js
@@ -0,0 +1,39 @@
+let x = 5;
+let y = 10;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+x = 'hello';
+y = 'world';
+console.log(x==y);
+
+y = 'hello';
+console.log(x ==y);
+
+x = 23;
+y = '23';
+console.log(x == y);
+console.log(x === y);
+console.log(x!==y);
+
+y =23;
+console.log(x===y);
+console.log(x < y);
+console.log(x > y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(x!=y);
+
+x = 123; y = 456;
+console.log(x,y);
+x += 5;
+console.log(x);
+console.log(x && y);
+console.log(x || y);
+console.log(x === y? 'hello': 'world');
+
+
+
