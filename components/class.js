307ae2d49ecd11f48de7d377c6962073a0a5f34f
Kumareshwari.k <141395010+Kumareshwari-0@users.noreply.github.com>
2024-01-18 10:38:46 +0530

    Add files via upload

---
+++ components/class.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Dora extends Component {
+    render() {
+        return (<h1>Function invoked through class</h1>);
+    }
+}
+export default Dora;
